3f6743cf82b009e8e449a4b7a3a2492bf9f84601
lismgmk <lismgmk2@gmail.com>
2022-11-02 19:44:09 +0300

    fix controller

--- src/modules/testing/testing.controller.ts
+++ src/modules/testing/testing.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Delete, HttpCode, UseFilters } from '@nestjs/common';
 import { MongoExceptionFilter } from '../../exceptions/mongoose-exception-filter';
 import { TestingService } from './testing.service';
 
-@Controller('testing/all-data')
+@Controller('testing')
 export class TestingController {
   constructor(private readonly testingService: TestingService) {}
 
-  @Delete()
+  @Delete('/all-data')
   @HttpCode(204)
   @UseFilters(new MongoExceptionFilter())
   async deleteAllData() {
