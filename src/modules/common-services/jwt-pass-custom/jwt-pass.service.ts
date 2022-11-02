a23a38252e49cef24d98c16df22f00a1113058a2
lismgmk <lismgmk2@gmail.com>
2022-11-02 15:33:21 +0300

    test 4.1

--- src/modules/common-services/jwt-pass-custom/jwt-pass.service.ts
+++ src/modules/common-services/jwt-pass-custom/jwt-pass.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { Types } from 'mongoose';
 
 @Injectable()
