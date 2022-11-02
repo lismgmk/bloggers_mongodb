2661cc38156ec5543f4e12ddeeaecee8cb6f1867
lismgmk <lismgmk2@gmail.com>
2022-11-02 18:58:34 +0300

    7.1

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import cookieParser from 'cookie-parser';
 
-import { MongoExceptionFilter } from './exceptions/mongoose-exception-filter';
+// import { MongoExceptionFilter } from './exceptions/mongoose-exception-filter';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  app.useGlobalFilters(new MongoExceptionFilter());
+  // app.useGlobalFilters(new MongoExceptionFilter());
   app.use(cookieParser());
   await app.listen(process.env.PORT || 5000);
 }
