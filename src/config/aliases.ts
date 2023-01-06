0409b0625f0612d450359952eace77c0f32986bd
lismgmk <lismgmk2@gmail.com>
2023-01-06 19:27:31 +0300

    fix-6

--- src/config/aliases.ts
+++ src/config/aliases.ts
@@ -1,7 +1,7 @@
 import moduleAlias from 'module-alias';
 import path from 'path';
 
-const rootPath = path.resolve(__dirname, '..', '..', 'dist');
+const rootPath = path.resolve(__dirname, '..', '..', 'dist/src/');
 moduleAlias.addAliases({
   '@app': rootPath,
 });
