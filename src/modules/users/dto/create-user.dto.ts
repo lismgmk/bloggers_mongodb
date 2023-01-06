e06dac9fb1742d571e785397692f0db9052c5472
lismgmk <lismgmk2@gmail.com>
2023-01-06 13:32:50 +0300

    fix-12

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -7,18 +7,18 @@ import {
 import { ForExistsUserError } from '../../../dto-validator/if-exist-user-drop-error';
 
 export class CreateUserDto {
-  @ForExistsUserError({
-    message: FIELD_EXIST_VALIDATION_ERROR,
-  })
+  // @ForExistsUserError({
+  //   message: FIELD_EXIST_VALIDATION_ERROR,
+  // })
   @Length(3, 10)
   readonly login: string;
 
   @Length(6, 20, { message: FIELD_LENGTH_VALIDATION_ERROR })
   readonly password: string;
 
-  @ForExistsUserError({
-    message: FIELD_EXIST_VALIDATION_ERROR,
-  })
+  // @ForExistsUserError({
+  //   message: FIELD_EXIST_VALIDATION_ERROR,
+  // })
   @IsEmail({ message: FIELD_EMAIL_VALIDATION_ERROR })
   readonly email: string;
 }
