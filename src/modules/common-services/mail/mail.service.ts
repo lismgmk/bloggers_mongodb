3e0070d4d91e3f3f72ec304cec9f0f6bf894dc84
lismgmk <lismgmk2@gmail.com>
2023-01-08 12:56:58 +0300

    fix html

--- src/modules/common-services/mail/mail.service.ts
+++ src/modules/common-services/mail/mail.service.ts
@@ -19,10 +19,7 @@ export class MailService {
       await this.mailerService.sendMail({
         to: user.email,
         subject: 'Welcome to Lis App! Confirm your Email',
-        html: ` <h1>Password recovery</h1>
-       <p>To finish password recovery please follow the link below:
-          <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
-      </p>`,
+        text: `https://somesite.com/password-recovery?recoveryCode=${code}`,
         // template: './confirmation', // `.hbs` extension is appended automatically
         context: {
           // ✏️ filling curly brackets with content
